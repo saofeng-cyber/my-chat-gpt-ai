e106b9ead9cc6951f0e1ff739f915a6b4ba9e013
sunminggang <942391953@qq.com>
2023-04-06 00:02:57 +0800

    init gpt api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello() {
-    return '欢迎进入OPENAI，请在法律合法的情况下使用，如果有问题请联系骚风(QQ:942391953)';
+    return '欢迎进入OPENAI，请在法律合法的情况下使用，如果有问题请联系骚风(QQ:3300626947)';
   }
 }
